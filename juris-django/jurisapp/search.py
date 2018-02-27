bbc333b480757817f7c82b3af8a7e12227c98a75
EllisBell <tee.vee.zed@gmail.com>
2018-02-27 21:26:09 +0000

    more fun with elastisearch

--- juris-django/jurisapp/search.py
+++ juris-django/jurisapp/search.py
@@ -8,13 +8,6 @@ def get_es():
     return es
 
 
-# analyzes text
-def test_analyse(analyser, text):
-    es = get_es()
-    index_client = es.indices
-    return index_client.analyze(body={"analyzer": analyser, "text": text})
-
-
 # indexing a doc using es.index would normally create index if didn't exist
 # but we want to specify analyzer for index so think I need to specifically create one with mappings etc.
 def create_acordao_idx():
@@ -92,22 +85,62 @@ def create_acordao_doc_with_desc(ac):
     return doc
 
 
-def index_acordao(ac_doc, ac_id):
+# interface
+def index_acordao(ac):
+    doc = create_acordao_doc_with_desc(ac)
+    index_acordao_with_id(doc, ac.acordao_id)
+
+
+def index_acordao_with_id(ac_doc, ac_id):
     es = get_es()
     es.index(index="acordao_idx", doc_type="acordao", body=ac_doc, id=ac_id)
 
 
+# interface
 def count_indexed_acordaos(es):
     return es.count(index="acordao_idx", doc_type="acordao")
 
 
 # Todo need something that will index on update of acordao table
 
-def search_all_fields(query, match_type, operator, return_source=True):
+
+## Search Interface ##
+
+# analyzes text
+def test_analyse(analyser, text):
+    es = get_es()
+    index_client = es.indices
+    return index_client.analyze(body={"analyzer": analyser, "text": text})
+
+
+# interface
+# Main search, called from view
+def search(query):
+    # get just ids
+    res = search_all_fields(query, "best_fields", "and", False)
+    return get_ids_from_res(res)
+
+
+def search_with_paging(query, page_number, display_size):
+    if not page_number:
+        page_number = 1
+
+    start = (page_number - 1) * display_size
+    res = search_all_fields(query, "best_fields", "and", False, start, display_size)
+    return get_ids_from_res(res)
+
+
+def get_ids_from_res(res):
+    res_data = res['hits']['hits']
+    ac_ids = [hit["_id"] for hit in res_data]
+    return ac_ids
+
+
+def search_all_fields(query, match_type, operator, return_source=True, start_at=0, res_size=10):
     es = get_es()
     fields = get_searchable_fields()
     body = get_multi_match_query(query, fields, match_type, operator)
-    res = es.search(index="acordao_idx", body=body, _source=return_source)
+    res = es.search(index="acordao_idx", body=body, _source=return_source, from_=start_at, size=res_size)
     return res
 
 
@@ -130,11 +163,50 @@ def get_multi_match_query(query, fields, match_type, operator):
     return query
 
 
+def get_multi_match_query_with_tribs(query, tribs, fields, match_type, operator):
+    query = {
+        "query": {
+            "bool": {
+                operator: {
+                    "multi_match": {
+                        "query": query,
+                        "fields": fields,
+                        "type": match_type
+                    }
+                },
+                "filter": {
+                    "terms": {
+                        "tribunal": tribs
+                    }
+                }
+            }
+        }
+    }
+
+    return query
+
+
+# todo complete
+def convert_es_res_to_acordaos(res):
+    res_data = res['hits']['hits']  # list of dicts
+    # Convert results to list of acordao objects with fields to display in search results
+    for ac_dict in res_data:
+        source = ac_dict['source']
+        ac = Acordao()
+        ac.processo = ac_dict["processo"]
+        ac.relator = ac_dict["relator"]
+        ac.tribunal = ac_dict["tribunal"]
+        ac.sumario = ac_dict["sumario"]
+        ac.txt_integral = ac_dict["txt_integral"]
+        ac.txt_parcial = ac_dict["txt_parcial"]
+        ac.data = ac_dict["data"]
+
+
 def search_field(query, field):
     return None
 
 
-def search():
+def asearch():
     return None
     # Definitely want to use multi match, question is what type
     # https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html
