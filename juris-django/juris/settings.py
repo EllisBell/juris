f054c9d4065daf1316f8b4eb47bf33c4f410b0a5
EllisBell <tee.vee.zed@gmail.com>
2018-03-17 07:58:26 +0000

    adding allowed hosts to settings

--- juris-django/juris/settings.py
+++ juris-django/juris/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('JURIS_SECRET', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['188.166.101.76', 'jurisprudencia.pt']
 
 
 # Application definition
