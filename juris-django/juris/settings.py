a20e6dde98a73eba29d9b763ec0d313fb423fa18
EllisBell <tee.vee.zed@gmail.com>
2018-03-17 18:42:37 +0000

    making debug flag an env variable

--- juris-django/juris/settings.py
+++ juris-django/juris/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('JURIS_SECRET', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['188.166.101.76', 'jurisprudencia.pt', '127.0.0.1']
 
