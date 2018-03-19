c70dad322fe9717ecf14091ad9f45b2537a938ad
EllisBell <tee.vee.zed@gmail.com>
2018-03-19 20:42:12 +0000

    testing scrape task scheduling

--- scraping/scrape_controller.py
+++ scraping/scrape_controller.py
@@ -1,6 +1,7 @@
 import trib_scraper as ts
 import acordao_scraper as acs
 import acordao_saver
+import time
 
 default_trib_dict = {
     "TRL": "/jtrl.nsf?OpenDatabase",
@@ -10,20 +11,23 @@ default_trib_dict = {
     "TRG": "/jtrg.nsf?OpenDatabase"
 }
 
+start_time = None
 
-def scrape_tribs(tribs = default_trib_dict):
+
+def scrape_tribs(tribs = default_trib_dict, time_limit=None):
+    start_time = time.time()
     for trib_id, url in tribs.items():
-        scrape_and_save_all(url, trib_id)
+        scrape_and_save_all(url, trib_id, time_limit)
 
 
 # for scraping and saving everything unsaved
 # call this for each trib you want to scrape
 # trib_url is something like "/jtrl.nsf?OpenDatabase"
-def scrape_and_save_all(trib_url, trib_id):
+def scrape_and_save_all(trib_url, trib_id, time_limit=None):
     # Should we pass count to scrape_and_save or just pass -1 by default to scrape_trib - to get everything?
     # Probably have another method which calls this passing in -1
     all_urls = ts.scrape_all(trib_url)
-    save_acordaos(all_urls, trib_id)
+    save_acordaos(all_urls, trib_id, time_limit)
 
 
 def scrape_and_save_exact(trib_url, trib_id, count, start):
@@ -31,12 +35,14 @@ def scrape_and_save_exact(trib_url, trib_id, count, start):
     save_acordaos(all_urls, trib_id)
 
 
-def save_acordaos(acordao_urls, trib_id):
+def save_acordaos(acordao_urls, trib_id, time_limit=None):
     ac_saver = acordao_saver.AcordaoSaver()
     currently_saved = ac_saver.get_currently_saved(trib_id)
 
     unsaved_urls = [url for url in acordao_urls if url not in currently_saved]
     for url in unsaved_urls:
+        if time_limit and ((time.time() - start_time) > time_limit):
+            break
         ac = acs.get_acordao(url, trib_id)
         if ac:
             ac_saver.save(ac)
---
+++ scraping/scrape_tasks.py
@@ -0,0 +1,31 @@
+from celery import Celery
+from celery.schedules import crontab
+import scrape_controller as sc
+
+app = Celery('scrape_tasks', broker='redis://localhost:6379')
+
+app.conf.update(
+    task_serializer='json',
+    accept_content=['json'],  # Ignore other content
+    result_serializer='json',
+    timezone='UTC',
+    enable_utc=True,
+)
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(30.0, test_scrape_task.s(''), name='print every 30')
+
+
+@app.task
+def run_scrape():
+    # time limit is in seconds
+    sc.scrape_tribs(time_limit=18000)
+
+
+@app.task
+def test_scrape_task():
+    # time limit is in seconds
+    print("I am a scrape task!")
