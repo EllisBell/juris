a570aabb8b3bc2959d04618ae4d9c0eb500b1ff5
EllisBell <tee.vee.zed@gmail.com>
2018-04-22 15:11:18 +0100

    filepath issues

--- juris-django/jurisapp/pdf.py
+++ juris-django/jurisapp/pdf.py
@@ -6,6 +6,6 @@ from django.conf import settings
 def get_acordao_pdf(ac, absolute_uri):
     html_template = render_to_string('jurisapp/acordao_pdf.html', {'acordao': ac})
     html = HTML(string=html_template, base_url=absolute_uri)
-    pdf = html.write_pdf(stylesheets=[CSS(settings.STATIC_ROOT + "jurisapp\css\pdf_style.css")])
+    pdf = html.write_pdf(stylesheets=[CSS(settings.STATIC_ROOT + "jurisapp/css/pdf_style.css")])
     #pdf = html.write_pdf(stylesheets=[CSS("static\jurisapp\css\pdf_style.css")])
     return pdf
